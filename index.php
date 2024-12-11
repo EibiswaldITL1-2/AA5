4406c829b527b1d4f57029900019702cd3e3ef13
Andy-Tho <andy1997.ak@gmail.com>
2024-12-11 11:08:03 +0100

    second commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WEBPHPSTORM</title>
+</head>
+<body>
+    <h1>Docker hihi</h1>
+    <p>
+        <?php
+            phpinfo();
+        ?>
+    </p>
+</body>
+</html>
